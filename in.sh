039609acd1943d0ed2477c82f9bbe5c03425b643
rawder <ppp7829@mail.ru>
2020-11-24 09:32:45 +0300

    Changed font-awesome to Roboto

--- in.sh
+++ in.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-sudo apt install -y crda curl diffutils dirmngr ethtool \
+sudo apt install -y alsa-utils rda curl diffutils dirmngr ethtool \
 		findutils firefox firefox-locale-ru \
-		fonts-font-awesome fonts-noto-hinted \
-		fonts-noto-ui-core fonts-noto-ui-extra \
-		fonts-ubuntu-console git gnupg \
+		fonts-font-awesome fonts-noto \
+		fonts-ubuntu-console fontconfig git gnupg \
 		gsettings-desktop-schemas gzip hostname htop \
 		jq kitty libdebconfclient0 libpam0g-dev \
 		libsodium23 libxcb-xkb-dev \
 		lm-sensors lvm2 lxd-agent-loader make \
 		mako-notifier mesa-utils mpd multipath-tools \
-		ncurses-base ncurses-bin neofetch pavucontrol pollinate python3-nacl \
+		ncurses-base ncurses-bin neofetch \
+		pavucontrol pollinate python3-nacl \
 		python3-pymacaroons read-edid shim-signed \
-		sway swayidle swaylock waybar wofi xcb xwayland \
-		alsa-utils
-
-cd ~/.vim/pack/themes/start && git clone --depth=1 https://github.com/dracula/vim.git dracula
+		sway swayidle swaylock waybar wofi xcb xwayland
 
 cd ~/.src && \
 	git clone --depth=1 https://github.com/nullgemm/ly.git && \
